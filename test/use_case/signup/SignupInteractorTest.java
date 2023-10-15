5833c8d08630994ce5fe595ec129d6e7dd8ca341
paulgries <pgries@cs.toronto.edu>
2023-10-15 13:16:50 -0400

    Added test for user already exists

--- test/use_case/signup/SignupInteractorTest.java
+++ test/use_case/signup/SignupInteractorTest.java
@@ -4,9 +4,6 @@ import data_access.InMemoryUserDataAccessObject;
 import entity.CommonUserFactory;
 import entity.User;
 import entity.UserFactory;
-import interface_adapter.signup.SignupPresenter;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
@@ -40,7 +37,8 @@ class SignupInteractorTest {
         interactor.execute(inputData);
     }
 
-    void failureTest() {
+    @Test
+    void failurePasswordMismatchTest() {
         SignupInputData inputData = new SignupInputData("Paul", "password", "wrong");
         SignupUserDataAccessInterface userRepository = new InMemoryUserDataAccessObject();
 
@@ -62,4 +60,31 @@ class SignupInteractorTest {
         interactor.execute(inputData);
     }
 
+    @Test
+    void failureUserExistsTest() {
+        SignupInputData inputData = new SignupInputData("Paul", "password", "wrong");
+        SignupUserDataAccessInterface userRepository = new InMemoryUserDataAccessObject();
+
+        // Add Paul to the repo so that when we check later they already exist
+        UserFactory factory = new CommonUserFactory();
+        User user = factory.create("Paul", "pwd",  LocalDateTime.now());
+        userRepository.save(user);
+
+        // This creates a presenter that tests whether the test case is as we expect.
+        SignupOutputBoundary failurePresenter = new SignupOutputBoundary() {
+            @Override
+            public void prepareSuccessView(SignupOutputData user) {
+                // 2 things to check: the output data is correct, and the user has been created in the DAO.
+                fail("Use case success is unexpected.");
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                assertEquals("User already exists.", error);
+            }
+        };
+
+        SignupInputBoundary interactor = new SignupInteractor(userRepository, failurePresenter, new CommonUserFactory());
+        interactor.execute(inputData);
+    }
 }
